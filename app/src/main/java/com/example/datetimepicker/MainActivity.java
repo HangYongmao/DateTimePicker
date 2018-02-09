21bf53919de5a9047124b3b6bf81919c5f25d294
HangYongmao <1981217256@qq.com>
2018-02-10 00:00:06 +0800

    在Title显示时间

--- app/src/main/java/com/example/datetimepicker/MainActivity.java
+++ app/src/main/java/com/example/datetimepicker/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.datetimepicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TimePicker timePicker;
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private int year;
+    private int month;
+    private int day;
+    private int hour;
+    private int minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 获取日历对象
+        calendar = Calendar.getInstance();
+        // 获取年月日时分秒信息
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH) + 1;
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        minute = calendar.get(Calendar.MINUTE);
+        setTitle(year + "-" + month + "-" + day + "-" + hour + "-" + minute);
     }
 }
